235449956453a005a683d615524aa856bf40ea3d
Vinay Maurya <Vinay34978@gmail.com>
2024-01-31 12:52:13 +0530

    Final touch added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TaskController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,19 +12,17 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('tasks/create', [\App\Http\Controllers\TaskController::class, 'create'])->name('tasks.create');
-Route::post('tasks', [\App\Http\Controllers\TaskController::class, 'store'])->name('tasks.store');
-
-Route::get('tasks', [\App\Http\Controllers\TaskController::class, 'index'])->name('tasks.index');
-Route::get('tasks/{task}', [\App\Http\Controllers\TaskController::class, 'show'])->name('tasks.show');
-
-Route::get('tasks/{task}/edit', [\App\Http\Controllers\TaskController::class, 'edit'])->name('tasks.edit');
-Route::put('tasks/{task}', [\App\Http\Controllers\TaskController::class, 'update'])->name('tasks.update');
-Route::patch('tasks/{task}/complete', [\App\Http\Controllers\TaskController::class, 'complete'])->name('tasks.complete');
-Route::patch('tasks/{task}/yet_complete', [\App\Http\Controllers\TaskController::class, 'yetComplete'])->name('tasks.yet_complete');
-
-Route::delete('tasks/{task}', [\App\Http\Controllers\TaskController::class, 'destroy'])->name('tasks.destroy');
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(TaskController::class)
+    ->prefix('tasks/{task}')
+    ->as('tasks.')
+    ->group(function () {
+        Route::patch('complete', 'complete')->name('complete');
+        Route::patch('yet_complete', 'yetComplete')->name('yet_complete');
+    });
+
+Route::resource('tasks', TaskController::class);
